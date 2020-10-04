09f309cac21668c80ff0f6121f61f4fa3fd27332
Napat <napat.wongchr@gmail.com>
2020-10-04 23:30:12 +0700

    refactor render props

--- src/FormRenderProps.js
+++ src/FormRenderProps.js
@@ -3,6 +3,7 @@ import "./Form.css";
 
 class Form extends React.Component {
   render() {
+    const { values, handleValueChange } = this.props;
     return (
       <div className="form-container">
         <label htmlFor="firstname">First Name</label>
@@ -10,16 +11,16 @@ class Form extends React.Component {
           id="firstname"
           name="firstname"
           type="text"
-          value={this.props.values.firstname}
-          onChange={this.props.handleValueChange}
+          value={values.firstname}
+          onChange={handleValueChange}
         />
         <label htmlFor="lastname">Last Name</label>
         <input
           id="lastname"
           name="lastname"
           type="text"
-          value={this.props.values.lastname}
-          onChange={this.props.handleValueChange}
+          value={values.lastname}
+          onChange={handleValueChange}
         />
       </div>
     );
